01cf40d7a5166e2830a9af8b412bcfda9330814e
Lillys21 <rahelish@hotmail.com>
2018-09-09 20:17:44 -0500

    Nuevos cambios js

--- js/marvel.js
+++ js/marvel.js
@@ -2,3 +2,10 @@ $(document).ready(function(){
     console.log(apiKey);
 
     var apiKey="0ab55f3eb3f88236784a773210cfad1b";
+
+    $("#search-button").click(function() {
+        console.log("botón de search clicleado");
+        var search = $("#search").val();
+        
+        var $characters = $("#characters");
+        $characters.html("<h1>Searching</h1>");
