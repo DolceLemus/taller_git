319c6d87e5020312e1b928ed1640fd1f1bc6c253
clau1889 <clau.blanco1889@gmail.com>
2018-09-09 20:17:52 -0500

    Más líneas

---
+++ js/index.js
@@ -0,0 +1,47 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDBgPgEjpEG4kZqyq1Vnw39bc3Rw-U-Hrc",
+    authDomain: "marvel-2.firebaseapp.com",
+    databaseURL: "https://marvel-2.firebaseio.com",
+    projectId: "marvel-2",
+    storageBucket: "marvel-2.appspot.com",
+    messagingSenderId: "668439989433"
+};
+firebase.initializeApp(config);
+
+/*var marvel_config = {
+    apiKey: "0ab55f3eb3f88236784a773210cfad1b",
+}; */
+
+
+$(document).ready(function(){
+    console.log('Página cargada');
+    
+    $("#login").click(function() {
+        console.log('Botón login clicleado');
+        var email = $("#email").val();
+        var password = $("#password").val();
+        
+                        
+        firebase.auth().signInWithEmailAndPassword(email, password)
+        .catch( function(error){
+            console.log(error);
+            alert(error.message);
+        } );
+    });
+    $("#logout").click(function() {
+        console.log('Botón logout clicleado');
+        firebase.auth().signOut();
+    });
+
+    $("#signup").click(function() {
+        console.log('Botón signup clicleado');
+        var email = $("#email").val();
+        var password = $("#password").val();
+        
+
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+        .catch( function(error){
+            console.log(error);
+            alert(error.message);
+        } );
